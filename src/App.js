0652ccaece78d961667efd8e8455a7de791453e5
Armando Rafael <armando.romero2020@hotmail.com>
2020-12-13 17:56:33 -0600

    creando un titulo y un parrafo inicial

--- src/App.js
+++ src/App.js
@@ -6,17 +6,11 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>hola mundo</p>
+        <div>
+            <h1>BUENOS DIAS..!!!</h1>
+            <h3>espero que tengas un buen dia</h3>
+        </div>
       </header>
     </div>
   );
